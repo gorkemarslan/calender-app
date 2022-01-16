8ad4734299c38bb845a09dfee159882e6f482c81
gorkemarslan <garslan13@gmail.com>
2022-01-16 23:34:03 +0300

    Add components

--- calender-app/src/App.js
+++ calender-app/src/App.js
@@ -1,9 +1,24 @@
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
 import { getMonth } from "./util";
 
 function App() {
-  console.table(getMonth());
-  return <div className="App"></div>;
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+
+  return (
+    <React.Fragment>
+      <div className="h-screen flex flex-col">
+        <Header />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default App;
