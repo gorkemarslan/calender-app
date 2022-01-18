b784fd3de73cb64fa817593170640663004cbfc0
gorkemarslan <garslan13@gmail.com>
2022-01-18 21:27:58 +0300

    Add colours to safelist

--- calender-app/tailwind.config.js
+++ calender-app/tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: [
+    "bg-indigo-500",
+    "bg-gray-500",
+    "bg-green-500",
+    "bg-blue-500",
+    "bg-red-500",
+    "bg-purple-500",
+  ],
   theme: {
     extend: {
       fontFamily: {
