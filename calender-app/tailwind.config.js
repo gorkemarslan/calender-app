64709f042e3cf1814e18f3345a7642bc4829c297
gorkemarslan <garslan13@gmail.com>
2022-01-19 19:44:16 +0300

    Extend safe list

--- calender-app/tailwind.config.js
+++ calender-app/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   safelist: [
+    "bg-indigo-200",
+    "bg-gray-200",
+    "bg-green-200",
+    "bg-blue-200",
+    "bg-red-200",
+    "bg-purple-200",
     "bg-indigo-500",
     "bg-gray-500",
     "bg-green-500",
