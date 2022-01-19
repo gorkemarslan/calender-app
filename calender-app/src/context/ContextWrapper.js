7817dac6848e7bd58415c30b4aaca35e88eefa65
gorkemarslan <garslan13@gmail.com>
2022-01-19 19:44:03 +0300

    Update global context

--- calender-app/src/context/ContextWrapper.js
+++ calender-app/src/context/ContextWrapper.js
@@ -1,18 +1,76 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer, useMemo } from "react";
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
+
+function savedEventReducer(state, { type, payload }) {
+  switch (type) {
+    case "push":
+      return [...state, payload];
+    case "update":
+      return state.map((evt) => (evt.id === payload.id ? payload : evt));
+    case "delete":
+      return state.filter((evt) => evt.id !== payload.id);
+    default:
+      throw new Error();
+  }
+}
+
+function initEvents() {
+  const storageEvents = localStorage.getItem("savedEvents");
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
+  return parsedEvents;
+}
 
 export default function ContextWrapper(props) {
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
   const [miniCalenderMonth, setMiniCalenderMonth] = useState(null);
   const [daySelected, setDaySelected] = useState(dayjs());
   const [showEventModal, setShowEventModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [labels, setLabels] = useState([]);
+  const [savedEvents, dispatchCallEvent] = useReducer(
+    savedEventReducer,
+    [],
+    initEvents
+  );
+
+  const filteredEvents = useMemo(() => {
+    return savedEvents.filter((evt) =>
+      labels
+        .filter((lbl) => lbl.checked)
+        .map((lbl) => lbl.label)
+        .includes(evt.label)
+    );
+  }, [savedEvents, labels]);
+
+  useEffect(() => {
+    localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+  }, [savedEvents]);
+
+  useEffect(() => {
+    setLabels((prevLabels) =>
+      [...new Set(savedEvents.map((evt) => evt.label))].map((label) => {
+        const currentLabel = prevLabels.find((lbl) => lbl.label === label);
+        return { label, checked: currentLabel ? currentLabel.checked : true };
+      })
+    );
+  }, [savedEvents]);
 
   useEffect(() => {
     if (miniCalenderMonth !== null) {
       setMonthIndex(miniCalenderMonth);
     }
   }, [miniCalenderMonth]);
+
+  useEffect(() => {
+    if (!setShowEventModal) {
+      setSelectedEvent(null);
+    }
+  }, [setShowEventModal]);
+
+  function updateLabel(label) {
+    setLabels(labels.map((lbl) => (lbl.label === label.label ? label : lbl)));
+  }
 
   return (
     <GlobalContext.Provider
@@ -25,6 +83,14 @@ export default function ContextWrapper(props) {
         setDaySelected,
         showEventModal,
         setShowEventModal,
+        dispatchCallEvent,
+        savedEvents,
+        selectedEvent,
+        setSelectedEvent,
+        labels,
+        setLabels,
+        updateLabel,
+        filteredEvents,
       }}
     >
       {props.children}
